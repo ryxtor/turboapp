5c387a4ad9194dd5055db9e65a102a4ef894af33
ryxtor <ryxtor@outlook.com>
2023-01-26 20:35:42 -0300

    basic flash

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -34,7 +34,8 @@ class MessagesController < ApplicationController
           render turbo_stream: [
             turbo_stream.update('new_message', partial: 'messages/form', locals: { message: Message.new }),
             turbo_stream.prepend('messages', partial: 'messages/message', locals: { message: @message }),
-            turbo_stream.update('message_count', html: Message.count)
+            turbo_stream.update('message_count', html: Message.count),
+            turbo_stream.update('notice', html: 'Message was successfully created.')
           ]
         end
         format.html { redirect_to message_url(@message), notice: "Message was successfully created." }
@@ -56,7 +57,10 @@ class MessagesController < ApplicationController
     respond_to do |format|
       if @message.update(message_params)
         format.turbo_stream do
-          render turbo_stream: turbo_stream.update(@message, partial: 'messages/message', locals: { message: @message })
+          render turbo_stream: [
+            turbo_stream.update(@message, partial: 'messages/message', locals: { message: @message }),
+            turbo_stream.update('notice', html: 'Message was successfully updated.')
+          ]
         end
         format.html { redirect_to message_url(@message), notice: "Message was successfully updated." }
         format.json { render :show, status: :ok, location: @message }
@@ -78,7 +82,8 @@ class MessagesController < ApplicationController
       format.turbo_stream do
         render turbo_stream: [
           turbo_stream.remove(@message),
-          turbo_stream.update('message_count', html: Message.count)
+          turbo_stream.update('message_count', html: Message.count),
+          turbo_stream.update('notice', html: 'Message was successfully deleted.')
         ]
       end
       format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
