011241dc9aae9ce03e58418036e31738866b86cb
ryxtor <ryxtor@outlook.com>
2023-01-26 20:13:06 -0300

    remove messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -63,6 +63,7 @@ class MessagesController < ApplicationController
     @message.destroy
 
     respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@message) }
       format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
       format.json { head :no_content }
     end
