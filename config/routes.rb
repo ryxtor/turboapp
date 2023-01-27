2d4ed7c4125e97c058cb2f1c8e13127d007e1518
ryxtor <ryxtor@outlook.com>
2023-01-27 18:38:35 -0300

    Add movies search bar with turbo and stimulus

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :movies do
+    collection do
+      post :search
+    end
+  end
   resources :messages do
     member do
       post :edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_26_223647) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_27_204716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "messages", force: :cascade do |t|
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
